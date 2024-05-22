149fc77b3e0d325a91c02c2dcc3f24741480e56a
Jeremy Blaker <jblaker@brightcove.com>
2024-05-22 10:00:13 -0400

    change download url

--- lib/utils.bash
+++ lib/utils.bash
@@ -40,8 +40,7 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# https://github.com/tomaz/appledoc/releases/download/2.2.1/appledoc-2.2.1.zip
-	url="$GH_REPO/releases/download/${version}/appledoc-${version}.zip"
+	url="https://solutions.brightcove.com/jblaker/appledoc-${version}.zip"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
