5a7b69d38e00361a490eb38f8b04a3e6dd6ccb98
Jeremy Blaker <jblaker@brightcove.com>
2024-05-22 10:03:31 -0400

    remove install command

--- lib/utils.bash
+++ lib/utils.bash
@@ -45,8 +45,6 @@ download_release() {
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
-
-	sudo sh install-appledoc.sh
 }
 
 install_version() {
