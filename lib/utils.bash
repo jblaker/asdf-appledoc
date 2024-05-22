e8069eda0d5c44e80ed83e8b8451b5f6090e9eaa
Jeremy Blaker <jblaker@brightcove.com>
2024-05-22 15:40:31 -0400

    argh

--- lib/utils.bash
+++ lib/utils.bash
@@ -45,8 +45,6 @@ download_release() {
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
-
-	unzip "$filename"
 }
 
 install_version() {
